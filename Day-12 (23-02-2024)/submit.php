265a38057b44c6021ee4923f0404cb8bd0c3980a
Milan Das <45796126+milandas63@users.noreply.github.com>
2024-02-23 14:57:37 +0530

    Add files via upload

--- Day-12 (23-02-2024)/submit.php
+++ Day-12 (23-02-2024)/submit.php
@@ -5,25 +5,25 @@ $username = "giftintern";
 $password = "password";
 
 // Create connection
-$conn = mysql_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password);
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-mysql_select_db('prateekshya');
+mysqli_select_db($conn, "prateekshya");
 
-$str=mysql_query("select * from employ");
+$str=mysqli_query($conn, "select * from employ");
 
-while($stp=mysql_fetch_array($str)) {
+while($stp=mysqli_fetch_array($str)) {
 	$emp_id=$stp['emp_id'];
 	$emp_name=$stp['emp_name'];
 	$emp_gender=$stp['emp_gender'];
 	$emp_dob=$stp['emp_dob'];
 	$emp_email=$stp['emp_email'];
 	$emp_mobile=$stp['emp_mobile'];
-
+	echo $emp_name . "<br>";
 }
 
 ?>
